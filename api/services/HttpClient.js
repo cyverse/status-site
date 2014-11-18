3122518d82fa072021e38dae017fffc7c071370d
jchansen <jason_hansen@outlook.com>
2014-11-18 15:45:13 -0700

    remove fake code

--- api/services/HttpClient.js
+++ api/services/HttpClient.js
@@ -5,34 +5,14 @@ var HttpClient = function(){
 };
 
 HttpClient.prototype.get = function(url, cb){
-  //request({
-  //  url: url,
-  //  method: "GET"
-  //}, function (error, response, body) {
-  //  if(error) return cb(error);
-  //  return cb(null, body);
-  //});
-
-  var body = {
-    result: {
-      status: [
-        {
-          id: "544e810a96cc7fe45400897b",
-          name: "Web Application",
-          containers: [
-            {
-              id: "544e810a96cc7fe45400897a",
-              name: "Tucson",
-              status: "Operational",
-              status_code: 100
-            }
-          ]
-        }
-      ]
-    }
-  };
-
-  cb(null, body)
+  request({
+    url: url,
+    method: "GET"
+  }, function (error, response, body) {
+    if(error) return cb(error);
+    var bodyJSON = JSON.parse(body);
+    return cb(null, bodyJSON);
+  });
 };
 
 module.exports = HttpClient;
