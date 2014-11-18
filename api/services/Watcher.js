078fef99f0945f36efbd73086766dba7e9422ceb
jchansen <jason_hansen@outlook.com>
2014-11-18 00:27:46 -0700

    refactor to use promises

--- api/services/Watcher.js
+++ api/services/Watcher.js
@@ -10,22 +10,46 @@ var Watcher = function(uow, statusChecker, statusReporter){
   this.statusReporter = statusReporter;
 };
 
+Watcher.prototype._check = function(url){
+  var statusChecker = this.statusChecker;
+
+  return new Promise(function(resolve, reject){
+    statusChecker.check(url, function(err, status){
+      if(err) return reject(err);
+      resolve(status);
+    });
+  })
+};
+
+Watcher.prototype._report = function(id, status){
+  var statusReporter = this.statusReporter;
+
+  return new Promise(function(resolve, reject){
+    statusReporter.report(id, status, function(err, response){
+      if(err) return reject(err);
+      resolve(response);
+    });
+  })
+};
+
+Watcher.prototype._waitUntilNextCheck = function(service){
+  setTimeout(function(){
+    this.watch(service);
+  }.bind(this), 1000);
+};
+
 Watcher.prototype.watch = function(service, cb){
   var statusChecker = this.statusChecker,
       statusReporter = this.statusReporter,
       that = this;
 
-  statusChecker.check(service.api, function(err, status){
-    if(err) return cb(err);
-
-    statusReporter.report(service.id, status, function(err, response){
-      if(err) return cb(err);
-
-      setTimeout(function(){
-        that.watch(service);
-      }, 1000);
-    });
-  });
+  this._check(service.api)
+      .then(function(status){
+        that._report(service.id, status)
+      })
+      .then(function(result){
+        that._waitUntilNextCheck(service);
+      });
 };
 
 module.exports = Watcher;
