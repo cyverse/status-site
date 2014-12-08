25cb450fb8e9971cbb51b05886d0b931fbb08459
jchansen <jason_hansen@outlook.com>
2014-12-08 10:34:02 -0700

    add mongolab db for heroku use

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,18 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  //someMongodbServer: {
+  //  adapter: 'sails-mongo',
+  //  host: 'localhost',
+  //  port: 27017,
+  //  // user: 'username',
+  //  // password: 'password',
+  //  // database: 'your_mongo_db_name_here'
+  //},
+
+  mongolab: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    url: process.env.MONGOLAB_URI
   },
 
   /***************************************************************************
