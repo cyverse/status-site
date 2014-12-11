8c732e850385b0ae070b311dcebaf9a7fa27ab59
loukitzanna <loukia@dhcp-10-142-199-232.uawifi.arizona.edu>
2014-12-11 14:50:13 -0700

    add logging for debugging

--- api/services/Watcher.js
+++ api/services/Watcher.js
@@ -1,5 +1,5 @@
 var Promise = require('bluebird');
-var pollingInterval = 1*1*1000; // poll every minute
+var pollingInterval = 1*10*1000; // poll every minute
 //var pollingInterval = 1*1*1000; // poll every minute
 //TODO but does this really poll every minute?
 
@@ -30,6 +30,7 @@ Watcher.prototype._check = function(service){
     var statusChecker = this.statusChecker;
 
     return new Promise(function(resolve, reject){
+        console.log("checking...");
         statusChecker.check(service, function(err, status){
             if(err) return reject(err);
             resolve(status);
@@ -49,6 +50,7 @@ Watcher.prototype._report = function(id, status){
 };
 
 Watcher.prototype._waitUntilNextCheck = function(service){
+    console.log("waiting...");
   setTimeout(function(){
     this.watch(service);
   }.bind(this), pollingInterval);
@@ -61,6 +63,7 @@ Watcher.prototype.watch = function(service, cb){
 
   this._check(service)
       .then(function(status){
+          console.log("reporting...");
         that._report(service.id, status)
       })
       .then(function(result){
